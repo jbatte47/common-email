464d6521254ec08934e91f5e46c134f6f78d7f79
unknown <john.batte@Server2008DevTest.startechnology.com>
2012-12-19 15:54:54 -0600

    fixing blocked constructor calls

--- src/Common.Email/InternetMessage.cs
+++ src/Common.Email/InternetMessage.cs
@@ -4,5 +4,20 @@ namespace Common.Email
 {
     public class InternetMessage : MailMessage
     {
+        public InternetMessage()
+        {
+        }
+
+        public InternetMessage(string from, string to) : base(from, to)
+        {
+        }
+
+        public InternetMessage(string from, string to, string subject, string body) : base(from, to, subject, body)
+        {
+        }
+
+        public InternetMessage(MailAddress from, MailAddress to) : base(from, to)
+        {
+        }
     }
 }
